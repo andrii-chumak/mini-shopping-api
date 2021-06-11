003ce71c9be949a67634706f24348c9a72755e20
Andrii Chumak <andrii.chumak@coxit.co>
2021-06-11 12:43:56 +0300

    Add user model and resource classes

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask
-from database import db
+from flask_restful import Api
 
+from resources.user import User, UserRegister
+
+from database import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+
+api = Api(app)
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserRegister, '/register')
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 
 if __name__ == '__main__':
--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from database import db
 class UserModel(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
     password = db.Column(db.String(40))
 
@@ -15,3 +15,17 @@ class UserModel(db.Model):
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
+
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'username': self.username
+        }
