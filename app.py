92187fd76fe3c9b3fa8311d71870366a43554efe
Andrii Chumak <andrii.chumak@coxit.co>
2021-06-11 11:34:50 +0300

    Add database initializing

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from database import db
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
 
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(port=5000, debug=True)
