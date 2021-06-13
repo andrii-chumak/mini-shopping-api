8035ad1b197e2246ac4703726a393ce30199f382
Andrii Chumak <andrii.chumak@coxit.co>
2021-06-13 18:34:08 +0300

    Add parser to UserRegister and method delete

--- resources/user.py
+++ resources/user.py
@@ -1,12 +1,24 @@
-from flask import request
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 from models.user import UserModel
 
 
 class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="Username is required field"
+                        )
+
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help="Password is required field"
+                        )
+
     def post(self):
-        data = request.get_json()
+        data = UserRegister.parser.parse_args()
 
         if UserModel.find_by_username(data['username']):
             return {'message': 'This username is already in use.'}, 400
@@ -18,11 +30,18 @@ class UserRegister(Resource):
 
 
 class User(Resource):
-    @classmethod
-    def get(cls, user_id):
+    def get(self, user_id):
         user = UserModel.find_by_id(user_id)
 
         if not user:
             return {'message': "User with id '{}' doesn't exist".format(user_id)}, 404
 
         return user.to_json()
+
+    def delete(self, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': "User with id '{}' doesn't exist".format(user_id)}, 404
+
+        user.delete_from_db()
+        return {'message': 'User deleted'}, 200
