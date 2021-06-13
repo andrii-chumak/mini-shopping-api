f79ebb249b7a6e8f36f79a407edcc7ef6312f5b7
andrii-chumak <74974402+andrii-chumak@users.noreply.github.com>
2021-06-13 18:11:22 +0300

    Revert "Improve orders"

--- models/order.py
+++ models/order.py
@@ -1,26 +1,6 @@
 from database import db
 
 
-class OrderStatus:
-    created = 1
-    payed = 2
-    packed = 3
-    shipped = 4
-    delivered = 5
-
-    status_dict = {
-        created: 'Created',
-        payed: 'Payed',
-        packed: 'Packed',
-        shipped: 'Shipped',
-        delivered: 'Delivered',
-    }
-
-    @classmethod
-    def to_text(cls, status):
-        return cls.status_dict[status]
-
-
 class OrderProduct(db.Model):
     __tablename__ = 'ordered_products'
 
@@ -28,26 +8,23 @@ class OrderProduct(db.Model):
     order_id = db.Column(db.Integer, db.ForeignKey('orders.id'))
     product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
     quantity = db.Column(db.Integer, default=1)
-    free_products = db.Column(db.Integer, default=0)
 
 
 class OrderModel(db.Model):
     __tablename__ = 'orders'
 
     id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.Integer)
+    status = db.Column(db.String(20))
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     user = db.relationship("UserModel", back_populates="orders")
 
     products = db.relationship('ProductModel', secondary='ordered_products', lazy='dynamic')
-    total = db.Column(db.Float(precision=2))
 
-    def __init__(self, user_id, products, total):
+    def __init__(self, user_id, products):
         self.user_id = user_id
-        self.total = total
+        self.status = 'Created'
         self.products = products
-        self.status = OrderStatus.created
 
     def save_to_db(self):
         db.session.add(self)
@@ -61,37 +38,13 @@ class OrderModel(db.Model):
         order_product = db.session.query(OrderProduct).filter_by(order_id=self.id).filter_by(product_id=product_id).first()
         return order_product.quantity
 
-    def set_quantity_by_product_id(self, product_id, value):
-        order_product = db.session.query(OrderProduct).filter_by(order_id=self.id).filter_by(product_id=product_id)
-        order_product.update({OrderProduct.quantity: value})
-        self.save_to_db()
-
-    def get_number_free_products(self, product_id):
-        order_product = db.session.query(OrderProduct).filter_by(order_id=self.id).filter_by(product_id=product_id).first()
-        return order_product.free_products
-
-    def set_number_free_products(self, product_id, value):
-        order_product = db.session.query(OrderProduct).filter_by(order_id=self.id).filter_by(product_id=product_id)
-        order_product.update({OrderProduct.free_products: value})
-        self.save_to_db()
-
     def to_json(self):
         products = [product.to_json() for product in self.products.all()]
         for product in products:
             product['quantity'] = self.get_quantity_by_product_id(product['id'])
-            product['free'] = self.get_number_free_products(product['id'])
 
         return {
             'id': self.id,
             'user_id': self.user_id,
-            'status': self.get_status(),
-            'products': products,
-            'total': self.total
+            'products': products
         }
-
-    def set_status(self, status):
-        self.status = status
-        self.save_to_db()
-
-    def get_status(self):
-        return OrderStatus.to_text(self.status)
--- resources/cart.py
+++ resources/cart.py
@@ -4,12 +4,6 @@ from models.cart import CartModel
 from models.product import ProductModel
 from models.user import UserModel
 
-from utils.cart_calculations import (
-    check_preconditions,
-    calculate_subtotal,
-    calculate_total,
-)
-
 
 class Cart(Resource):
     parser = reqparse.RequestParser()
@@ -36,18 +30,11 @@ class Cart(Resource):
         cart = CartModel.find_by_user_id(user_id)
 
         product = ProductModel.find_by_id(data['product_id'])
-        if not product:
-            return {'message': "This product doesn't exist"}
-
-        cart.add_product(product)
-        result, message = check_preconditions(cart)
-
-        if result:
+        if product:
+            cart.add_product(product)
             return cart.to_json()
 
-        cart.remove_product(product)
-        return {'message': message}
-
+        return {'message': "This product doesn't exist"}
 
     def delete(self, user_id):
         user = UserModel.find_by_id(user_id)
@@ -58,22 +45,3 @@ class Cart(Resource):
         cart.delete_all_from_cart()
 
         return {'message': "The cart was cleaned"}, 404
-
-
-class CartTotal(Resource):
-    def get(self, user_id):
-        user = UserModel.find_by_id(user_id)
-        if not user:
-            return {'message': "This user doesn't exist"}, 404
-
-        cart = CartModel.find_by_user_id(user_id)
-        products = [product.to_json() for product in cart.products.all()]
-        for product in products:
-            product['quantity'] = cart.get_quantity_by_product_id(product['id'])
-            product['free'] = cart.get_number_free_products(product['id'])
-
-        return {
-            'products': products,
-            'subtotal': calculate_subtotal(cart),
-            'total': calculate_total(cart)
-        }
