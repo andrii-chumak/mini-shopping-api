a87356aecf28a42727b50ac40020278337f137f0
Andrii Chumak <andrii.chumak@coxit.co>
2021-06-14 00:57:20 +0300

    Add basic test for user resources

---
+++ tests/resources/resource_user_test.py
@@ -0,0 +1,40 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask.testing import FlaskClient
+from tests.fixtures import app, db, client
+from models.user import UserModel
+from resources.user import User
+
+
+def test_get(db: SQLAlchemy):
+    user = UserModel(username="TestUser", password='test1234')
+
+    db.session.add(user)
+    db.session.commit()
+
+    result = User.get(1)
+
+    assert result['username'] == user.username
+
+
+def test_delete(db: SQLAlchemy, client: FlaskClient):
+    user = UserModel(username="TestUser2", password='test')
+
+    db.session.add(user)
+    db.session.commit()
+
+    result = client.delete(
+        '/user/1',
+    )
+
+    assert result.status_code == 200
+
+
+def test_register(client: FlaskClient):
+    payload = dict(username='new_user', password='new_user')
+    client.post(
+        '/register',
+        json=payload
+    ).get_json()
+
+    result = client.get('/user/1').get_json()
+    assert result['username'] == payload['username']
